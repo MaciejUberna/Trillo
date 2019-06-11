71e635620c117409f88c7a1645fcbb13e0183925
Maciej Uberna <maciej.uberna@gmail.com>
2019-06-11 15:30:41 +0200

    Gulp libs + flexbox Cheatsheet

---
+++ starter/gulpfile.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var gulp = require('gulp'),
+sass = require('gulp-sass'),
+concat = require('gulp-concat'),
+wait = require('gulp-wait'),
+autoprefixer = require('gulp-autoprefixer'),
+gls = require('gulp-live-server');
+
+function checkHTML(html){
+  console.log("Html "+html+" zmieniony.");
+}
+
+// Define tasks after requiring dependencies
+function style() {
+
+  // Where should gulp look for the sass files?
+  // My .sass files are stored in the styles folder
+  // (If you want to use scss files, simply look for *.scss files instead)
+  return (
+      gulp
+          .src(['sass/main.scss'])
+          .pipe(wait(200))
+
+          // Use sass with the files found, and log any errors
+          .pipe( sass({outputStyle: 'compressed', errLogToConsole: true }) )
+          .on("error", sass.logError)
+
+          .pipe(concat('style.css'))
+
+          // -webkit- and so on
+          .pipe(autoprefixer({browsers: ['last 10 versions'],cascade: false}))
+
+          // What is the destination for the compiled file?
+          .pipe(gulp.dest("css"))
+  );
+}
+
+function watchFiles(){
+    // var server = gls.static(); //equals to gls.static('public', 3000);
+    // server.start();
+
+  // gulp.watch takes in the location of the files to watch for changes
+  // and the name of the function we want to run on change
+  gulp.watch('sass/**', style);
+  gulp.watch('index.html',checkHTML('index.html'));
+//   gulp.watch(['css/**/*.css', 'index.html'], function (file) {
+//     server.notify.apply(server, [file]);
+//   });
+}
+
+// Expose the task by exporting it
+// This allows you to run it from the commandline using
+// $ gulp style
+exports.style = style;
+
+// Don't forget to expose the task!
+exports.watchFiles = watchFiles
+
+exports.checkHTML = checkHTML;
